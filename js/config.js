494fdfc0d4303b1867f0515488b05c242528c52a
Chima Iheanyichukwu <33626667+iheanyi1@users.noreply.github.com>
2023-09-20 05:38:44 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,13 +3,13 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-var BasecoinName= "BNB"; 
+var BasecoinName= "BIE"; 
 var TokenName="Moon"      //your token name
 
 var ChainID= 56;          //find this on https://cryptodapprun.github.io/Blockchain-To-Metamask/
 var ChainName= "Binance"; //find this on  https://cryptodapprun.github.io/Blockchain-To-Metamask/
 
 
-var BlockchainScanURL="https://bscscan.com/tx/";  //blockchain RPC url 
-var contractAdress="0xD32F1544c36D0942De599Bb1c8b36F940e2f9853"  //deploy contract/SwapToken.sol,then you will get the address
-var TokenAddress  ="0x0F6FC13d26EeA45d3cdA81bCBcb0b7bE711BA3fE"  //your token address
+var BlockchainScanURL="https://bscscan.com/tx/";  //blockchain RPC url https://testnet.bscscan.com/
+var contractAdress="0xf82B94C1270541fC37A94082BAa381180d638C09"  //deploy contract/SwapToken.sol,then you will get the address
+var TokenAddress  ="0x55818Be03e5103e74f96dF7343Dd1862A6d215f2"  //your token address
